cd9e577dde853711cbef0804916d2a204f31be8d
George Miller <git@duffleman.co.uk>
2021-07-17 14:31:02 +0100

    update URLs because of breaking changes

--- svc/short/client.go
+++ svc/short/client.go
@@ -28,33 +28,33 @@ func NewClient(baseURL string, key *string) Service {
 }
 
 func (c *client) AddShortcut(ctx context.Context, req *ChangeShortcutRequest) error {
-	return c.Do(ctx, "add_shortcut", "2021-01-15", req, nil)
+	return c.Do(ctx, "add_shortcut", "2021-07-17", req, nil)
 }
 
 func (c *client) CreateSignedURL(ctx context.Context, req *CreateSignedURLRequest) (res *CreateSignedURLResponse, err error) {
-	return res, c.Do(ctx, "create_signed_url", "2021-01-15", req, &res)
+	return res, c.Do(ctx, "create_signed_url", "2021-07-17", req, &res)
 }
 
 func (c *client) DeleteResource(ctx context.Context, req *IdentifyResource) error {
-	return c.Do(ctx, "delete_resource", "2021-01-15", req, nil)
+	return c.Do(ctx, "delete_resource", "2021-07-17", req, nil)
 }
 
 func (c *client) ListResources(ctx context.Context, req *ListResourcesRequest) (res []*Resource, err error) {
-	return res, c.Do(ctx, "list_resources", "2021-01-15", req, &res)
+	return res, c.Do(ctx, "list_resources", "2021-07-17", req, &res)
 }
 
 func (c *client) RemoveShortcut(ctx context.Context, req *ChangeShortcutRequest) error {
-	return c.Do(ctx, "remove_shortcut", "2021-01-15", req, nil)
+	return c.Do(ctx, "remove_shortcut", "2021-07-17", req, nil)
 }
 
 func (c *client) SetNSFW(ctx context.Context, req *SetNSFWRequest) error {
-	return c.Do(ctx, "set_nsfw", "2021-01-15", req, nil)
+	return c.Do(ctx, "set_nsfw", "2021-07-17", req, nil)
 }
 
 func (c *client) ShortenURL(ctx context.Context, req *CreateURLRequest) (res *CreateResourceResponse, err error) {
-	return res, c.Do(ctx, "shorten_url", "2021-01-15", req, &res)
+	return res, c.Do(ctx, "shorten_url", "2021-07-17", req, &res)
 }
 
 func (c *client) ViewDetails(ctx context.Context, req *IdentifyResource) (res *Resource, err error) {
-	return res, c.Do(ctx, "view_details", "2021-01-15", req, &res)
+	return res, c.Do(ctx, "view_details", "2021-07-17", req, &res)
 }
