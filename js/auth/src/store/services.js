c3014a27005b607059a39810c3c678a380ad2376
Laura Miller <git@duffle.one>
2021-08-08 16:50:11 +0100

    dynamic URL changing for local dev

--- js/auth/src/store/services.js
+++ js/auth/src/store/services.js
@@ -1,8 +1,15 @@
 import crpc from 'crpc';
 
-// const auth = crpc('https://auth.dfl.mn');
-const auth = crpc('http://localhost:3000');
+const auth = crpc(getAuthURL());
 
 export default {
 	auth,
 };
+
+function getAuthURL() {
+	// eslint-disable-next-line no-undef
+	if (location.hostname.includes('localhost'))
+		return 'http://localhost:3000';
+
+	return 'https://api.duffle.one/1/auth';
+}
